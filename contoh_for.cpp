879c40f5272c43e70f85734737af80f2f02aac9b
Nur-Sidik <nursidik1500@gmail.com>
2025-03-17 15:39:18 +0700

    menambahkan for pencacah naik

--- contoh_for.cpp
+++ contoh_for.cpp
@@ -8,4 +8,15 @@ int main()
     int n;
     int PerulanganWhile = 0;
     int PerulanganDo = 0;
+
+    cout << "Perulangan dengan for pencacah naik" << endl;
+    for (n = 0; n < 5; n++)
+    {
+        cout << "Nilai n; " << n << "Selamat Datang" << endl;
+    }
+    cout << "Nilai n terakhir = " << n << endl;
+    cout << endl;
+    
+    cout << "Perulangan for pencacah turun" << endl;
+
 }
